58bc0ba1c675149e3ac6778d32b2dc42d6427024
Henrik-Ehde <henrik_ehde@hotmail.com>
2024-02-14 16:28:08 +0100

    Add project files.

---
+++ Asset.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTracking
+{
+    abstract class Asset
+    {
+        public Asset(string brand, DateOnly purchaseDate, int price, string office)
+        {
+            Price = price;
+            Brand = brand;
+            PurchaseDate = purchaseDate;
+            ExpireTime = purchaseDate.AddYears(3);
+            Office = office;
+        }        
+        public Asset(string brand, string purchaseDate, int price, string office)
+        {
+            try
+            {
+                PurchaseDate = DateOnly.Parse(purchaseDate);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("An invalid date was entered");
+            }
+
+            Price = price;
+            Brand = brand;            
+            ExpireTime = PurchaseDate.AddYears(3);
+            Office = office;
+        }
+
+        public int Price { get; set; }
+        public string Brand { get; set; }
+        public DateOnly PurchaseDate { get; set; }
+        public DateOnly ExpireTime { get; set; }
+        public string Office { get; set; }
+
+    }
+
+    class Laptop : Asset
+    {
+        public Laptop( string brand, DateOnly purchaseDate, int price, string office) : base(brand, purchaseDate, price, office) { }
+        public Laptop( string brand, string purchaseDate, int price, string office) : base(brand, purchaseDate, price, office) { }
+    }
+    
+    class Phone : Asset
+    {
+        public Phone( string brand, DateOnly purchaseDate, int price, string office) : base(brand, purchaseDate, price, office) { }
+        public Phone( string brand, string purchaseDate, int price, string office) : base(brand, purchaseDate, price, office) { }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using AssetTracking;
+
+List<Asset> assets = new List<Asset>()
+{
+    new Laptop("Asus", "2024-01-15", 500, "Skövde"),
+    new Laptop("Macintosh", "2022-05-17", 1000, "Skövde"),
+    new Laptop("HP", "01/26/2023", 800, "Brussels"),
+
+    new Phone("Nokia", "23-08-27", 200, "New York"),
+    new Laptop("Lenovo", "03/19/2021", 900, "New York")
+};
+
+PrintList();
+
+void PrintList()
+{
+    List<Asset> assetsSorted = assets.OrderBy(a => a.Office).ThenBy(a => a.PurchaseDate).ToList();
+    Console.WriteLine("Type".PadRight(16) + "Brand".PadRight(16) + "Office".PadRight(16) + "Purchase Date".PadRight(16) + "Price");
+    Console.WriteLine("----".PadRight(16) + "-----".PadRight(16) + "------".PadRight(16) + "-------------".PadRight(16) + "-----");
+
+    foreach (Asset a in assetsSorted)
+    {
+        string type = a.GetType().Name;
+        string pDate = a.PurchaseDate.ToString();
+        Console.WriteLine(type.PadRight(16) + a.Brand.PadRight(16) + a.Office.PadRight(16) + pDate.PadRight(16) + a.Price);
+    }
+}
+
+
+
+
+Console.ReadLine();
+
+
