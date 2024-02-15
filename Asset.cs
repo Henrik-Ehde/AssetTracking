98338c144ef13e88b9d3c1315c381410b171bc3a
Henrik-Ehde <henrik_ehde@hotmail.com>
2024-02-15 14:22:41 +0100

    Assets are displayed in a different color to indicate if they are expiring soon or already expired

--- Asset.cs
+++ Asset.cs
@@ -9,21 +9,14 @@ namespace AssetTracking
     abstract class Asset
     {
         //Stores all the information about an asset.
-        //One constructor recieves DateOnly. The other recieves a string and tries to convert it to a date.
+        //The constructor recieves a string and tries to convert it to a date.
         //Assets have an expiration date which is 3 years after their purchase date.
-        public Asset(string brand, DateOnly purchaseDate, int price, Office office)
-        {
-            Price = price;
-            Brand = brand;
-            PurchaseDate = purchaseDate;
-            ExpirationDate = purchaseDate.AddYears(3);
-            Office = office;
-        }        
+
         public Asset(string brand, string purchaseDate, int price, Office office)
         {
             try
             {
-                PurchaseDate = DateOnly.Parse(purchaseDate);
+                PurchaseDate = DateTime.Parse(purchaseDate);
             }
             catch (Exception)
             {
@@ -39,23 +32,20 @@ namespace AssetTracking
 
         public int Price { get; set; }
         public string Brand { get; set; }
-        public DateOnly PurchaseDate { get; set; }
-        public DateOnly ExpirationDate { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
         public Office Office { get; set; }
 
     }
 
     //Different types of assets. The Constructors use the base constructor.
-
     class Laptop : Asset
     {
-        public Laptop( string brand, DateOnly purchaseDate, int price, Office office) : base(brand, purchaseDate, price, office) { }
         public Laptop( string brand, string purchaseDate, int price, Office office) : base(brand, purchaseDate, price, office) { }
     }
     
     class Phone : Asset
     {
-        public Phone( string brand, DateOnly purchaseDate, int price, Office office) : base(brand, purchaseDate, price, office) { }
         public Phone( string brand, string purchaseDate, int price, Office office) : base(brand, purchaseDate, price, office) { }
     }
 
--- Program.cs
+++ Program.cs
@@ -1,20 +1,22 @@
 ﻿using AssetTracking;
 
 //Create some offices with their local currency's code and the exchange rate from USD to local currency
-Office skövde = new Office("Skövde", "SEK", 10.49);
-Office london = new Office("London", "GDP", 0.8);
-Office newYork = new Office("New York", "USD", 1.00);
+Office skövde = new("Skövde", "SEK", 10.49);
+Office london = new("London", "GDP", 0.8);
+Office newYork = new("New York", "USD", 1.00);
 
 
 //Create a populated list of assets
 List<Asset> assets =
 [
     new Laptop("Asus", "2024-01-15", 500, skövde),
-    new Laptop("Macintosh", "2022-05-17", 1000, skövde),
+    new Laptop("Macintosh", "2021-05-20", 1000, skövde),
     new Laptop("HP", "01/26/2023", 800, london),
+    new Laptop("Lenovo", "03/19/2021", 900, newYork),
 
     new Phone("Nokia", "23-08-27", 200, newYork),
-    new Laptop("Lenovo", "03/19/2021", 900, newYork)
+    new Phone ("Motorola", "2020-12-24", 300, london),
+    new Phone ("Iphone", "2021-06-20", 1250, london)
 ];
 
 //Display the list of assets
@@ -37,15 +39,26 @@ void PrintList()
 
     foreach (Asset a in assetsSorted)
     {
-        //Get the name of the asset's class in order to display what type of an asset it is
-        string type = a.GetType().Name;
-        // Calculate the assets price in the local currency of the office it belongs to
+        // Calculate the asset's price in the local currency.
         int localPrice = (int)(a.Price * a.Office.ExchangeRate);
-        //Calculate the time remaining until the expiration date of the asset.
-        TimeSpan timeLeft = DateOnly.FromDateTime(DateTime.Now) - a.ExpirationDate;
 
-        Console.WriteLine(type.PadRight(16) + a.Brand.PadRight(16) + a.Office.Name.PadRight(16) + a.PurchaseDate.ToString().PadRight(16)
-                            + a.Price.ToString().PadRight(16) + a.Office.currencyCode.PadRight(16) + localPrice);
+        string stringToWrite = a.GetType().Name.PadRight(16) + a.Brand.PadRight(16) + a.Office.Name.PadRight(16)
+                                + a.PurchaseDate.ToString("yyyy-MM-dd").PadRight(16) + a.Price.ToString().PadRight(16)
+                                + a.Office.currencyCode.PadRight(16) + localPrice.ToString().PadRight(10);
+
+        //Calculate the time remaining until the expiration date of the asset.
+        //Indicate if the asset is expiring soon or has expired by displaying in different colors and adding text.
+        TimeSpan timeLeft = a.ExpirationDate - DateTime.Now;
+        if (timeLeft.Days < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            stringToWrite += "EXPIRED";
+        }
+        else if (timeLeft.Days < 90) Console.ForegroundColor = ConsoleColor.Red;
+        else if (timeLeft.Days < 180) Console.ForegroundColor = ConsoleColor.Yellow;
+
+        Console.WriteLine(stringToWrite);
+        Console.ResetColor();
     }
 }
 
